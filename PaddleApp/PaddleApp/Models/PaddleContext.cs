fc7ca6a9b64ee3455d3a7cc876380dfcf9ed9049
Devashree A <devashree2911@gmail.com>
2021-12-05 16:20:37 +0530

    It's an application used to book courts.

---
+++ PaddleApp/PaddleApp/Models/PaddleContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PaddleApp.Models
+{
+    public class PaddleContext : DbContext
+    {
+
+        public PaddleContext() : base("PaddleConnection")
+        { 
+
+        }
+            public DbSet<Paddle> Location { get; set; }
+            public DbSet<Court> Court { get; set; }
+            public DbSet<Booking> Booking { get; set; }
+
+        
+    }
+}
